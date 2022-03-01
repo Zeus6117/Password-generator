18ad95a0585b59b00bb9e5705862bc6ffeb41fd5
Jordan King <96736562+Zeus6117@users.noreply.github.com>
2022-03-01 09:05:34 -0500

    after tutor seesion

--- Develop/script.js
+++ Develop/script.js
@@ -1,86 +1,131 @@
 // Assignment code here
+// var passwordLength = 8 < 128;
+// var password = "";
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var special = "!@#$%^&*()_+?><:{}[]";
-var passwordLength = 8 < 128;
-var password = "";
+function generatePassword() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()_+?><:{}[]";
 
-//1. Prompt the user for the password criteria
-var question1 = window.prompt("Would you like lower case characters? Enter YES or NO.")
-if (question1 === "YES" || question1 === "yes"); {
-lowerCase()
-}
- if (question1 === "NO" || question1 === "no"); {
-  
-}
-if (question1 === "" || question1 === null); {
-  window.alert("You need to provide a valid answer! Please try again."); 
-  return question1()
-};
-//console.log(question1);
+  var selectedSet = "";
 
-var question2 = window.prompt("Would you like upper case characters? Enter YES or NO."); {
-if (question2 === "YES" || question2 === "yes"); 
-  upperCase()
-}
- if (question2 === "NO" || question2 === "no"); {
 
-}
-if (question2 === "" || question2 === null); {
-  window.alert("You need to provide a valid answer! Please try again."); 
-  //return question2()
-};
-console.log(question2);
+  var passwordLength = window.prompt("How many characters would you like?");
+  var generatedPassword = "";
 
-var question3 = window.prompt("Would you like numbers? Enter YES or NO.")
-if (question3 === "YES" || question3 === "yes"); {
-  numbers()
-}
- if (question3 === "NO" || question3 === "no"); {
-
-}
-if (question3 === "" || question3 === null); {
-  window.alert("You need to provide a valid answer! Please try again."); 
-  //return question3()
-};
-console.log(question3);
-
-var question4 = window.prompt("Would you like special characters? Enter YES or NO.")
-if (question4 === "YES" || question4 === "yes"); {
-  special()
-}
- if (question4 === "NO" || question4 === "no"); {
-
-}
-if (question4 === "" || question4 === null); {
-  window.alert("You need to provide a valid answer! Please try again."); 
-  //return question4()
-};
-console.log(question4);
-
-var question5 = window.prompt("How many characters would you like?")
-
-if ( question5 === "" || question5 === null) {
-  window.alert("You need to provide a valid answer! Please try again."); 
-};
-
-console.log(question5);
-
-var generate = function() {
-  
-  function generatePassword() {
-  for (var i = 0; i < passwordLength; i++){
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
+  while (passwordLength === "" || passwordLength === null) {
+    window.alert("You need to provide a valid answer! Please try again.");
+    passwordLength = window.prompt("How many characters would you like?");
   }
+
+  ////////////////////////////////////////
+
+  //1. Prompt the user for the password criteria
+  var question1 = window.confirm(
+    "Would you like lower case characters? Enter YES or NO."
+  );
+  // if (question1 === "NO" || question1 === "no");
+  // {
+  // }
+  // if (question1 === "" || question1 === null);
+  // {
+  //   window.alert("You need to provide a valid answer! Please try again.");
+  //   return question1();
+  // }
+  // //console.log(question1);
+
+  var question2 = window.confirm(
+    "Would you like upper case characters? Enter YES or NO."
+  );
+  // {
+  //   if (question2 === "YES" || question2 === "yes");
+  //   upperCase();
+  // }
+  // if (question2 === "NO" || question2 === "no");
+  // {
+  // }
+  // if (question2 === "" || question2 === null);
+  // {
+  //   window.alert("You need to provide a valid answer! Please try again.");
+  //   //return question2()
+  // }
+  // console.log(question2);
+
+  var question3 = window.confirm("Would you like numbers? Enter YES or NO.");
+  
+  // if (question3 === "YES" || question3 === "yes");
+  // {
+  //   numbers();
+  // }
+  // if (question3 === "NO" || question3 === "no");
+  // {
+  // }
+  // if (question3 === "" || question3 === null);
+  // {
+  //   window.alert("You need to provide a valid answer! Please try again.");
+  //   //return question3()
+  // }
+  // console.log(question3);
+
+  var question4 = window.confirm(
+    "Would you like special characters? Enter YES or NO."
+  );
+  
+  // if (question4 === "YES" || question4 === "yes");
+  // {
+  //   special();
+  // }
+  // if (question4 === "NO" || question4 === "no");
+  // {
+  // }
+  // if (question4 === "" || question4 === null);
+  // {
+  //   window.alert("You need to provide a valid answer! Please try again.");
+  //   //return question4()
+  // }
+  // console.log(question4);
+
+  ////////////////////////////
+  console.log(question1)
+  console.log(question2)
+  console.log(question3)
+  console.log(question4)
+  
+  if (question1 == true)
+  {
+    selectedSet = selectedSet + lowerCase;
+  }
+
+  if (question2 == true)
+  {
+    selectedSet = selectedSet + upperCase;
+  }
+
+  if (question3 == true)
+  {
+    selectedSet = selectedSet + numbers;
+  }
+
+  if (question4 == true)
+  {
+    selectedSet = selectedSet + special;
+  }
+  ////////////////////////////////////////
+
+  for (var i = 0; i < passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * selectedSet.length);
+    // password += chars.substring(randomNumber, randomNumber + 1);
+
+    generatedPassword = generatedPassword + selectedSet[randomNumber];
+  }
+
+  console.log(generatedPassword);
   //2. Validate the input
-  //3. Generate password based in criteria.
+  //3. Generate password based in criteria.ha
 
-
-//Display generated password to the page
-  return "";
+  //Display generated password to the page
+  return generatedPassword;
 }
 
 // Get references to the #generate element
@@ -92,8 +137,7 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);}
+generateBtn.addEventListener("click", writePassword);
